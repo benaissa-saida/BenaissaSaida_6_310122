f9582f90d596a8f56cb63f13b0df57c9e53beafc
benaissa-saida <benaissasaida28@gmail.com>
2022-02-07 18:52:34 +0100

    factory + scss modif

--- scripts/factories/media.js
+++ scripts/factories/media.js
@@ -4,39 +4,46 @@ function mediaFactory(data) {
   const picture = `assets/images/${photographerId}/${image}`;
   const media = `assets/images/${photographerId}/${video}`;
 
-  // const photographe
-
-  function getPortfolioDOM() {
-    const figure = document.createElement("figure");
-    figure.className = "portfolio__info"
-
+  function createImage() {
     const img = document.createElement("img");
     img.className = "";
     img.setAttribute("src", picture);
     img.setAttribute("alt", title);
+    return img
+  }
 
+  function createVideo() {
     const video = document.createElement("video");
-    video.setAttribute("src", media);
-    video.setAttribute("preload", true);
-    video.setAttribute("loop", "");
+    // video.setAttribute('controls', '')
+    const source = document.createElement('source');
+    source.setAttribute("src", media);
+    source.setAttribute("preload", true);
+    source.setAttribute("loop", "");
+    video.appendChild(source)
+    return video
+  }
+
+  function getPortfolioDOM() {
+    const figure = document.createElement("figure");
+    figure.className = "portfolio__info";
 
     const figcaption = document.createElement("figcaption");
-    figcaption.className = "portfolio__info--caption"
+    figcaption.className = "portfolio__info--caption";
     figcaption.textContent = title;
 
     const like = document.createElement("span");
-    like.textContent = likes
+    like.textContent = likes;
 
-    const heart = document.createElement('i');
-    heart.className = "hearts far fa-heart"
-    
-    like.appendChild(heart)
-    figcaption.append(like)
-    
+    const heart = document.createElement("i");
+    heart.className = "hearts far fa-heart";
+
+    like.appendChild(heart);
+    figcaption.append(like);
+
     if (image) {
-      figure.append(img, figcaption);
+      figure.append(createImage(), figcaption);
     } else {
-      figure.append(video, figcaption);
+      figure.append(createVideo(), figcaption);
     }
     return figure;
   }
